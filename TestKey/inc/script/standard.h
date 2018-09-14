7a8b6dd4bec9b56b9de9bd345fffe531b8d958be
babylco0 <604666459@qq.com>
2018-09-14 15:38:09 +0800

    脚本生成测试程序
    在公私钥测试基础上，添加脚本生成测试程序。

---
+++ TestKey/inc/script/standard.h
@@ -0,0 +1,278 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2017 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_SCRIPT_STANDARD_H
+#define BITCOIN_SCRIPT_STANDARD_H
+
+#include <pubkey.h>
+
+//#include <script/interpreter.h>
+#include <uint256.h>
+
+#include <boost/variant.hpp>
+
+#include <stdint.h>
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////// 
+/** Script verification flags.
+ *
+ *  All flags are intended to be soft forks: the set of acceptable scripts under
+ *  flags (A | B) is a subset of the acceptable scripts under flag (A).
+ */
+enum
+{
+    SCRIPT_VERIFY_NONE      = 0,
+
+    // Evaluate P2SH subscripts (BIP16).
+    SCRIPT_VERIFY_P2SH      = (1U << 0),
+
+    // Passing a non-strict-DER signature or one with undefined hashtype to a checksig operation causes script failure.
+    // Evaluating a pubkey that is not (0x04 + 64 bytes) or (0x02 or 0x03 + 32 bytes) by checksig causes script failure.
+    // (not used or intended as a consensus rule).
+    SCRIPT_VERIFY_STRICTENC = (1U << 1),
+
+    // Passing a non-strict-DER signature to a checksig operation causes script failure (BIP62 rule 1)
+    SCRIPT_VERIFY_DERSIG    = (1U << 2),
+
+    // Passing a non-strict-DER signature or one with S > order/2 to a checksig operation causes script failure
+    // (BIP62 rule 5).
+    SCRIPT_VERIFY_LOW_S     = (1U << 3),
+
+    // verify dummy stack item consumed by CHECKMULTISIG is of zero-length (BIP62 rule 7).
+    SCRIPT_VERIFY_NULLDUMMY = (1U << 4),
+
+    // Using a non-push operator in the scriptSig causes script failure (BIP62 rule 2).
+    SCRIPT_VERIFY_SIGPUSHONLY = (1U << 5),
+
+    // Require minimal encodings for all push operations (OP_0... OP_16, OP_1NEGATE where possible, direct
+    // pushes up to 75 bytes, OP_PUSHDATA up to 255 bytes, OP_PUSHDATA2 for anything larger). Evaluating
+    // any other push causes the script to fail (BIP62 rule 3).
+    // In addition, whenever a stack element is interpreted as a number, it must be of minimal length (BIP62 rule 4).
+    SCRIPT_VERIFY_MINIMALDATA = (1U << 6),
+
+    // Discourage use of NOPs reserved for upgrades (NOP1-10)
+    //
+    // Provided so that nodes can avoid accepting or mining transactions
+    // containing executed NOP's whose meaning may change after a soft-fork,
+    // thus rendering the script invalid; with this flag set executing
+    // discouraged NOPs fails the script. This verification flag will never be
+    // a mandatory flag applied to scripts in a block. NOPs that are not
+    // executed, e.g.  within an unexecuted IF ENDIF block, are *not* rejected.
+    // NOPs that have associated forks to give them new meaning (CLTV, CSV)
+    // are not subject to this rule.
+    SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_NOPS  = (1U << 7),
+
+    // Require that only a single stack element remains after evaluation. This changes the success criterion from
+    // "At least one stack element must remain, and when interpreted as a boolean, it must be true" to
+    // "Exactly one stack element must remain, and when interpreted as a boolean, it must be true".
+    // (BIP62 rule 6)
+    // Note: CLEANSTACK should never be used without P2SH or WITNESS.
+    SCRIPT_VERIFY_CLEANSTACK = (1U << 8),
+
+    // Verify CHECKLOCKTIMEVERIFY
+    //
+    // See BIP65 for details.
+    SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY = (1U << 9),
+
+    // support CHECKSEQUENCEVERIFY opcode
+    //
+    // See BIP112 for details
+    SCRIPT_VERIFY_CHECKSEQUENCEVERIFY = (1U << 10),
+
+    // Support segregated witness
+    //
+    SCRIPT_VERIFY_WITNESS = (1U << 11),
+
+    // Making v1-v16 witness program non-standard
+    //
+    SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM = (1U << 12),
+
+    // Segwit script only: Require the argument of OP_IF/NOTIF to be exactly 0x01 or empty vector
+    //
+    SCRIPT_VERIFY_MINIMALIF = (1U << 13),
+
+    // Signature(s) must be empty vector if an CHECK(MULTI)SIG operation failed
+    //
+    SCRIPT_VERIFY_NULLFAIL = (1U << 14),
+
+    // Public keys in segregated witness scripts must be compressed
+    //
+    SCRIPT_VERIFY_WITNESS_PUBKEYTYPE = (1U << 15),
+};
+///////////////////////////////////////////////////////////////////////////////////////////////////// 
+
+static const bool DEFAULT_ACCEPT_DATACARRIER = true;
+
+class CKeyID;
+class CScript;
+
+/** A reference to a CScript: the Hash160 of its serialization (see script.h) */
+class CScriptID : public uint160
+{
+public:
+    CScriptID() : uint160() {}
+    CScriptID(const CScript& in);
+    CScriptID(const uint160& in) : uint160(in) {}
+};
+
+/**
+ * Default setting for nMaxDatacarrierBytes. 80 bytes of data, +1 for OP_RETURN,
+ * +2 for the pushdata opcodes.
+ */
+static const unsigned int MAX_OP_RETURN_RELAY = 83;
+
+/**
+ * A data carrying output is an unspendable output containing data. The script
+ * type is designated as TX_NULL_DATA.
+ */
+extern bool fAcceptDatacarrier;
+
+/** Maximum size of TX_NULL_DATA scripts that this node considers standard. */
+extern unsigned nMaxDatacarrierBytes;
+
+/**
+ * Mandatory script verification flags that all new blocks must comply with for
+ * them to be valid. (but old blocks may not comply with) Currently just P2SH,
+ * but in the future other flags may be added, such as a soft-fork to enforce
+ * strict DER encoding.
+ *
+ * Failing one of these tests may trigger a DoS ban - see CheckInputs() for
+ * details.
+ */
+static const unsigned int MANDATORY_SCRIPT_VERIFY_FLAGS = SCRIPT_VERIFY_P2SH;
+
+enum txnouttype
+{
+    TX_NONSTANDARD,
+    // 'standard' transaction types:
+    TX_PUBKEY,
+    TX_PUBKEYHASH,
+    TX_SCRIPTHASH,
+    TX_MULTISIG,
+    TX_NULL_DATA, //!< unspendable OP_RETURN script that carries data
+    TX_WITNESS_V0_SCRIPTHASH,
+    TX_WITNESS_V0_KEYHASH,
+    TX_WITNESS_UNKNOWN, //!< Only for Witness versions not already defined above
+};
+
+class CNoDestination {
+public:
+    friend bool operator==(const CNoDestination &a, const CNoDestination &b) { return true; }
+    friend bool operator<(const CNoDestination &a, const CNoDestination &b) { return true; }
+};
+
+struct WitnessV0ScriptHash : public uint256
+{
+    WitnessV0ScriptHash() : uint256() {}
+    explicit WitnessV0ScriptHash(const uint256& hash) : uint256(hash) {}
+    using uint256::uint256;
+};
+
+struct WitnessV0KeyHash : public uint160
+{
+    WitnessV0KeyHash() : uint160() {}
+    explicit WitnessV0KeyHash(const uint160& hash) : uint160(hash) {}
+    using uint160::uint160;
+};
+
+//! CTxDestination subtype to encode any future Witness version
+struct WitnessUnknown
+{
+    unsigned int version;
+    unsigned int length;
+    unsigned char program[40];
+
+    friend bool operator==(const WitnessUnknown& w1, const WitnessUnknown& w2) {
+        if (w1.version != w2.version) return false;
+        if (w1.length != w2.length) return false;
+        return std::equal(w1.program, w1.program + w1.length, w2.program);
+    }
+
+    friend bool operator<(const WitnessUnknown& w1, const WitnessUnknown& w2) {
+        if (w1.version < w2.version) return true;
+        if (w1.version > w2.version) return false;
+        if (w1.length < w2.length) return true;
+        if (w1.length > w2.length) return false;
+        return std::lexicographical_compare(w1.program, w1.program + w1.length, w2.program, w2.program + w2.length);
+    }
+};
+
+/**
+ * A txout script template with a specific destination. It is either:
+ *  * CNoDestination: no destination set
+ *  * CKeyID: TX_PUBKEYHASH destination (P2PKH)
+ *  * CScriptID: TX_SCRIPTHASH destination (P2SH)
+ *  * WitnessV0ScriptHash: TX_WITNESS_V0_SCRIPTHASH destination (P2WSH)
+ *  * WitnessV0KeyHash: TX_WITNESS_V0_KEYHASH destination (P2WPKH)
+ *  * WitnessUnknown: TX_WITNESS_UNKNOWN destination (P2W???)
+ *  A CTxDestination is the internal data type encoded in a bitcoin address
+ */
+typedef boost::variant<CNoDestination, CKeyID, CScriptID, WitnessV0ScriptHash, WitnessV0KeyHash, WitnessUnknown> CTxDestination;
+
+/** Check whether a CTxDestination is a CNoDestination. */
+bool IsValidDestination(const CTxDestination& dest);
+
+/** Get the name of a txnouttype as a C string, or nullptr if unknown. */
+const char* GetTxnOutputType(txnouttype t);
+
+/**
+ * Parse a scriptPubKey and identify script type for standard scripts. If
+ * successful, returns script type and parsed pubkeys or hashes, depending on
+ * the type. For example, for a P2SH script, vSolutionsRet will contain the
+ * script hash, for P2PKH it will contain the key hash, etc.
+ *
+ * @param[in]   scriptPubKey   Script to parse
+ * @param[out]  typeRet        The script type
+ * @param[out]  vSolutionsRet  Vector of parsed pubkeys and hashes
+ * @return                     True if script matches standard template
+ */
+bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<std::vector<unsigned char> >& vSolutionsRet);
+
+/**
+ * Parse a standard scriptPubKey for the destination address. Assigns result to
+ * the addressRet parameter and returns true if successful. For multisig
+ * scripts, instead use ExtractDestinations. Currently only works for P2PK,
+ * P2PKH, P2SH, P2WPKH, and P2WSH scripts.
+ */
+bool ExtractDestination(const CScript& scriptPubKey, CTxDestination& addressRet);
+
+/**
+ * Parse a standard scriptPubKey with one or more destination addresses. For
+ * multisig scripts, this populates the addressRet vector with the pubkey IDs
+ * and nRequiredRet with the n required to spend. For other destinations,
+ * addressRet is populated with a single value and nRequiredRet is set to 1.
+ * Returns true if successful. Currently does not extract address from
+ * pay-to-witness scripts.
+ *
+ * Note: this function confuses destinations (a subset of CScripts that are
+ * encodable as an address) with key identifiers (of keys involved in a
+ * CScript), and its use should be phased out.
+ */
+bool ExtractDestinations(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<CTxDestination>& addressRet, int& nRequiredRet);
+
+/**
+ * Generate a Bitcoin scriptPubKey for the given CTxDestination. Returns a P2PKH
+ * script for a CKeyID destination, a P2SH script for a CScriptID, and an empty
+ * script for CNoDestination.
+ */
+CScript GetScriptForDestination(const CTxDestination& dest);
+
+/** Generate a P2PK script for the given pubkey. */
+CScript GetScriptForRawPubKey(const CPubKey& pubkey);
+
+/** Generate a multisig script. */
+CScript GetScriptForMultisig(int nRequired, const std::vector<CPubKey>& keys);
+
+/**
+ * Generate a pay-to-witness script for the given redeem script. If the redeem
+ * script is P2PK or P2PKH, this returns a P2WPKH script, otherwise it returns a
+ * P2WSH script.
+ *
+ * TODO: replace calls to GetScriptForWitness with GetScriptForDestination using
+ * the various witness-specific CTxDestination subtypes.
+ */
+CScript GetScriptForWitness(const CScript& redeemscript);
+
+#endif // BITCOIN_SCRIPT_STANDARD_H
--- TestKey/main.cpp
+++ TestKey/main.cpp
@@ -4,124 +4,195 @@
 #include <key.h>
 #include <pubkey.h>
 #include <uint256.h>
+#include <script/standard.h>
+#include <script/script.h>
 
-int main(int argc, char** argv) {
-	//9a9a6539856be209b8ea2adbd155c0919646d108515b60b7b13d6a79f1ae5174
-	unsigned char testPriKey[32] = {0x9a, 0x9a, 0x65, 0x39, 0x85, 0x6b, 0xe2, 0x09,
-								0xb8, 0xea, 0x2a, 0xdb, 0xd1, 0x55, 0xc0, 0x91,
-								0x96, 0x46, 0xd1, 0x08, 0x51, 0x5b, 0x60, 0xb7,
-								0xb1, 0x3d, 0x6a, 0x79, 0xf1, 0xae, 0x51, 0x74};
-	//0340a609475afa1f9a784cad0db5d5ba7dbaab2147a5d7b9bbde4d1334a0e40a5e
-	std::vector<unsigned char> testPubKey = {0x03, 0x40, 0xa6, 0x09, 0x47, 0x5a, 0xfa, 0x1f, 0x9a,
-								0x78, 0x4c, 0xad, 0x0d, 0xb5, 0xd5, 0xba, 0x7d, 
-								0xba, 0xab, 0x21, 0x47, 0xa5, 0xd7, 0xb9, 0xbb, 
-								0xde, 0x4d, 0x13, 0x34, 0xa0, 0xe4, 0x0a, 0x5e};
-	CPrivKey LoadPriKey;
-	 
+// 打印n个x字符 
+void print_line(int n, char x){
+	std::cout<<std::endl<<std::setw(n);
+    std::cout<<std::setfill(x)<<" "<<std::endl;
+}
+
+/**
+ * 测试密钥对
+ * 私钥：9a9a6539856be209b8ea2adbd155c0919646d108515b60b7b13d6a79f1ae5174
+ * 公钥：0340a609475afa1f9a784cad0db5d5ba7dbaab2147a5d7b9bbde4d1334a0e40a5e
+ */ 
+std::vector<unsigned char> TestPriKey = {
+										0x9a, 0x9a, 0x65, 0x39, 0x85, 0x6b, 0xe2, 0x09,
+										0xb8, 0xea, 0x2a, 0xdb, 0xd1, 0x55, 0xc0, 0x91,
+										0x96, 0x46, 0xd1, 0x08, 0x51, 0x5b, 0x60, 0xb7,
+										0xb1, 0x3d, 0x6a, 0x79, 0xf1, 0xae, 0x51, 0x74
+										};
+std::vector<unsigned char> TestPubKey = {0x03, 
+										0x40, 0xa6, 0x09, 0x47, 0x5a, 0xfa, 0x1f, 0x9a,
+										0x78, 0x4c, 0xad, 0x0d, 0xb5, 0xd5, 0xba, 0x7d, 
+										0xba, 0xab, 0x21, 0x47, 0xa5, 0xd7, 0xb9, 0xbb, 
+										0xde, 0x4d, 0x13, 0x34, 0xa0, 0xe4, 0x0a, 0x5e
+										};
+/**
+ * Alice:
+ * PriKey: 6bce49ff84094382d822178de357a435f85e8ad8fdfa39bd58aa2e5192267198
+ * PubKey: 03e3bd2f408e4415aa57c747f6550937823a8605706c358facdc6325b4a99f2161
+ */
+std::vector<unsigned char> TestPriKeyA = {
+										0x6b, 0xce, 0x49, 0xff, 0x84, 0x09, 0x43, 0x82, 
+										0xd8, 0x22, 0x17, 0x8d, 0xe3, 0x57, 0xa4, 0x35, 
+										0xf8, 0x5e, 0x8a, 0xd8, 0xfd, 0xfa, 0x39, 0xbd, 
+										0x58, 0xaa, 0x2e, 0x51, 0x92, 0x26, 0x71, 0x98
+										};
+std::vector<unsigned char> TestPubKeyA = {0x03, 
+										0xe3, 0xbd, 0x2f, 0x40, 0x8e, 0x44, 0x15, 0xaa, 
+										0x57, 0xc7, 0x47, 0xf6, 0x55, 0x09, 0x37, 0x82, 
+										0x3a, 0x86, 0x05, 0x70, 0x6c, 0x35, 0x8f, 0xac, 
+										0xdc, 0x63, 0x25, 0xb4, 0xa9, 0x9f, 0x21, 0x61
+										};
+/**
+ * Bob:
+ * PriKey: 3e46c724c8e9728379a9cab2ec46030563f06d7e0ace2b734b5861b8f64b6f2f
+ * PubKey: 020e80933a750e84b4c35c10bc797ca34d1c885e4e65531a7499170a1c78ffdd97
+ */	
+std::vector<unsigned char> TestPriKeyB = {
+										0x3e, 0x46, 0xc7, 0x24, 0xc8, 0xe9, 0x72, 0x83, 
+										0x79, 0xa9, 0xca, 0xb2, 0xec, 0x46, 0x03, 0x05, 
+										0x63, 0xf0, 0x6d, 0x7e, 0x0a, 0xce, 0x2b, 0x73, 
+										0x4b, 0x58, 0x61, 0xb8, 0xf6, 0x4b, 0x6f, 0x2f
+										};								
+std::vector<unsigned char> TestPubKeyB = {0x02, 
+										0x0e, 0x80, 0x93, 0x3a, 0x75, 0x0e, 0x84, 0xb4, 
+										0xc3, 0x5c, 0x10, 0xbc, 0x79, 0x7c, 0xa3, 0x4d, 
+										0x1c, 0x88, 0x5e, 0x4e, 0x65, 0x53, 0x1a, 0x74, 
+										0x99, 0x17, 0x0a, 0x1c, 0x78, 0xff, 0xdd, 0x97
+										};
+/**
+ * Martin:
+ * PriKey: bfe5647b87c61058969bbe599879c49372fa0d6e2bfc71125960e3c1de5fbc0b
+ * PubKey: 039155f9024807d126be4df4d09273c5fece4767e89b4527c68b48414a2877eddd
+ */	
+std::vector<unsigned char> TestPriKeyM = {
+										0xbf, 0xe5, 0x64, 0x7b, 0x87, 0xc6, 0x10, 0x58, 
+										0x96, 0x9b, 0xbe, 0x59, 0x98, 0x79, 0xc4, 0x93, 
+										0x72, 0xfa, 0x0d, 0x6e, 0x2b, 0xfc, 0x71, 0x12, 
+										0x59, 0x60, 0xe3, 0xc1, 0xde, 0x5f, 0xbc, 0x0b
+										};								
+std::vector<unsigned char> TestPubKeyM = {0x03, 
+										0x91, 0x55, 0xf9, 0x02, 0x48, 0x07, 0xd1, 0x26, 
+										0xbe, 0x4d, 0xf4, 0xd0, 0x92, 0x73, 0xc5, 0xfe, 
+										0xce, 0x47, 0x67, 0xe8, 0x9b, 0x45, 0x27, 0xc6, 
+										0x8b, 0x48, 0x41, 0x4a, 0x28, 0x77, 0xed, 0xdd
+										};																				
+
+// 公私钥测试程序 
+int main_keytest() {
+
+	CPrivKey LoadPriKey;	 
 	CKey priKey;
 	//priKey.MakeNewKey(true);
 	ECCVerifyHandle eccVerifyHandle ;
 	ECC_Start();
-	// ����˽Կ���� 
-	priKey.Set(testPriKey, testPriKey+sizeof(testPriKey), true);	
+	// 设置私钥数据 
+	priKey.Set(TestPriKey.begin(), TestPriKey.end(), true);	
 	LoadPriKey = priKey.GetPrivKey();
 	std::cout<<std::setw(2)<<std::hex;
     std::cout<<std::setfill('0');
-	// ��ӡ˽Կ 
+	// 打印私钥 
 	std::cout<<"Private key [0x"<<priKey.size()<<"]:";
 	for(int ii = 0; ii < priKey.size(); ii++)
     {
         std::cout<<std::setw(2)<<std::hex;
         std::cout<<(int)(*(priKey.begin() + ii));
     }
-	std::cout<<std::endl;
-	// ��ȡ��Կ 
-	CPubKey pubKey(testPubKey);	
-	// ��ӡ��Կ 
+    print_line(20, '-');
+	// 获取公钥 
+	CPubKey pubKey(TestPubKey);	
+	// 打印公钥 
 	std::cout<<"Public key  [0x"<<pubKey.size()<<"]:";
 	for(int ii = 0; ii < pubKey.size(); ii++)
     {
         std::cout<<std::setw(2);
         std::cout<<std::setfill('0')<<std::hex<<(int)(*(pubKey.begin() + ii));
     }
-	std::cout<<std::endl;
-	// ��⹫˽Կ�Ƿ�ƥ�� 
+	print_line(20, '-');
+	// 检测公私钥是否匹配 
 	if(priKey.VerifyPubKey(pubKey)){
-		std::cout<<"Private Key vs. Public Key Match."<<std::endl;
+		std::cout<<"***** Private Key vs. Public Key Match. *****";
 	}
-	// ��ȡ��Կ Hash-160
-	uint160 hash160 = pubKey.GetID(); 
-	// ��ӡ��Կ Hash-160
+	print_line(20, '-');
+	// 获取公钥 Hash-160
+	CKeyID hash160 = pubKey.GetID(); 
+	// 打印公钥 Hash-160
 	std::cout<<"Hash-160    [0x"<<hash160.size()<<"]:";
 	for(int ii = 0; ii < hash160.size(); ii++)
     {
         std::cout<<std::setw(2);
         std::cout<<std::setfill('0')<<std::hex<<(int)(*(hash160.begin() + ii));
     }
-	std::cout<<std::endl;
-	// ��ȡ��Կ��ϣֵ 
+	print_line(20, '-');
+	// 获取公钥哈希值 
 	uint256 hash = pubKey.GetHash();
-	// ��ӡ��Կ��ϣֵ  
+	// 打印公钥哈希值  
 	std::cout<<"Hash        [0x"<<hash.size()<<"]:";
 	for(int ii = 0; ii < hash.size(); ii++)
     {
         std::cout<<std::setw(2);
         std::cout<<std::setfill('0')<<std::hex<<(int)(*(hash.begin() + ii));
     }
-	std::cout<<std::endl;
+	print_line(20, '-');
 	std::vector<unsigned char> vchSig; 
 	priKey.Sign(hash, vchSig);
-	// ��ӡǩ��
+	// 打印签名
 	std::cout<<"Sign        [0x"<<vchSig.size()<<"]:"; 
 	for(int ii = 0; ii < vchSig.size(); ii++){
 		std::cout<<std::setw(2);
         std::cout<<std::setfill('0')<<std::hex<<(int)(*(vchSig.begin() + ii));
 	}
-	std::cout<<std::endl;
+	print_line(20, '-');
 	
-	// У��ǩ�� 
+	// 校验签名 
 	if(pubKey.CheckLowS(vchSig) && pubKey.Verify(hash, vchSig)){
-		std::cout<<"Public Key Signed."<<std::endl;
+		std::cout<<"**** Public Key Verify Sign Ok. ****";
 	}
-	// ��ȡ��ѹ����Կ 
+	print_line(20, '-');
+	// 获取非压缩公钥 
 	pubKey.Decompress();
-	// ��ӡ��Կ 
+	// 打印公钥 
+	std::cout<<"**** Public key Decompressed ****"<<std::endl;
 	std::cout<<"Public key  [0x"<<pubKey.size()<<"]:";
 	for(int ii = 0; ii < pubKey.size(); ii++)
     {
         std::cout<<std::setw(2);
         std::cout<<std::setfill('0')<<std::hex<<(int)(*(pubKey.begin() + ii));
     }
-	std::cout<<std::endl;
+	print_line(20, '-');
 	
 	priKey.SignCompact(hash, vchSig);
 	pubKey.RecoverCompact(hash, vchSig);
-	// ��ӡ��Կ 
-	std::cout<<"Public key Recovered From SignCompact  [0x"<<pubKey.size()<<"]:";
+	// 打印公钥 
+	std::cout<<"**** Public key Recovered From SignCompact ****"<<std::endl;
+	std::cout<<"Public key  [0x"<<pubKey.size()<<"]:";
 	for(int ii = 0; ii < pubKey.size(); ii++)
     {
         std::cout<<std::setw(2);
         std::cout<<std::setfill('0')<<std::hex<<(int)(*(pubKey.begin() + ii));
     }
-	std::cout<<std::endl;
+	print_line(20, '-');
 	if(priKey.Load(LoadPriKey, pubKey, true)){
-		std::cout<<"Load private key ok."<<std::endl;
+		std::cout<<"**** Load private key ok. ****";
 	}
-	// ��ȡ��˽Կ 
+	print_line(20, '-');
+	// 获取子私钥 
 	CKey keyChild;
 	CPubKey pubkeyChild;
 	ChainCode ccChild;
 	unsigned int nChild = 1;
 	ChainCode cc = hash;
-	std::cout<<std::setw(100);
-    std::cout<<std::endl<<std::setfill('=')<<"="<<std::endl;
 	while(nChild <= 5){
 		std::cout<<"Child key: [0x";
 		std::cout<<std::setw(2);
-        std::cout<<std::setfill('0')<<std::hex<<nChild<<"]"<<std::endl;
-        std::cout<<"------------------------------------"<<std::endl;
+        std::cout<<std::setfill('0')<<std::hex<<nChild<<"]";
+        print_line(20, '-');
 		if(priKey.Derive(keyChild, ccChild, nChild, cc)){
-			// ��ӡ˽Կ 
+			// 打印私钥 
 			std::cout<<"Private key  [0x"<<pubKey.size()<<"]:";
 			for(int ii = 0; ii < keyChild.size(); ii++)
 		    {
@@ -131,25 +202,116 @@ int main(int argc, char** argv) {
 		    std::cout<<std::endl;
 		}
 		if(pubKey.Derive(pubkeyChild, ccChild, nChild, cc)){
-			// ��ӡ��Կ 
+			// 打印公钥 
 			std::cout<<"Public key   [0x"<<pubkeyChild.size()<<"]:";
 			for(int ii = 0; ii < pubkeyChild.size(); ii++)
 		    {
 		        std::cout<<std::setw(2);
 		        std::cout<<std::setfill('0')<<std::hex<<(int)(*(pubkeyChild.begin() + ii));
 		    }
-			std::cout<<std::endl;
 		}
-		// ��֤��Կ˽Կ�Ƿ�ƥ�� 
+		// 验证公钥私钥是否匹配 
 		if(!priKey.VerifyPubKey(pubKey)){
 			break;
 		}
 		cc = ccChild;
 		nChild++;
-		std::cout<<"------------------------------------"<<std::endl;
+		print_line(20, '-');
 	}	
-	std::cout<<std::setw(100);
-    std::cout<<std::endl<<std::setfill('=')<<"="<<std::endl;
 	ECC_Stop();
+	return 0;
+}
+/* 公私钥测试程序结束 */ 
+
+// 脚本生成测试程序
+int main_scripttest(){	
+	// 设置公钥 
+	CPubKey pubKey(TestPubKey);	
+	/** 从公钥生成一个P2PK脚本 */
+	CScript P2PK = GetScriptForRawPubKey(pubKey);
+	std::cout<<"**** P2PK:"<<std::endl;
+	for(int ii = 0; ii < P2PK.size(); ii++){
+		std::cout<<std::setw(2);
+		std::cout<<std::setfill('0')<<std::hex<<(int)(*(P2PK.begin() + ii));
+	}
+	print_line(20, '-'); 
+	/** 生成一个P2WPKH脚本 */
+	CScript P2WPKH = GetScriptForWitness(P2PK);
+	std::cout<<"**** P2WPKH:"<<std::endl;	
+	for(int ii = 0; ii < P2WPKH.size(); ii++){
+		std::cout<<std::setw(2);
+		std::cout<<std::setfill('0')<<std::hex<<(int)(*(P2WPKH.begin() + ii));
+	}
+	print_line(20, '-');
+	/* 生成一个脚本公钥 */
+	CScript scriptPubKey = GetScriptForDestination(P2WPKH);
+	std::cout<<"**** Bitcoin scriptPubKey:"<<std::endl;
+	if(!scriptPubKey.IsPayToScriptHash()){
+		std::cout<<" Error :";
+	}
+	for(int ii = 0; ii < scriptPubKey.size(); ii++){
+		std::cout<<std::setw(2);
+		std::cout<<std::setfill('0')<<std::hex<<(int)(*(scriptPubKey.begin() + ii));
+	}
+	print_line(20, '-'); 
+	/** 生成一个多签名脚本 */
+	std::vector<CPubKey> keys ={CPubKey(TestPubKeyA), CPubKey(TestPubKeyB), CPubKey(TestPubKeyM)};
+	CScript mnScript = GetScriptForMultisig(2, keys);
+	std::cout<<"**** Multisig script:"<<std::endl;
+	for(int ii = 0; ii < mnScript.size(); ii++){
+		std::cout<<std::setw(2);
+		std::cout<<std::setfill('0')<<std::hex<<(int)(*(mnScript.begin() + ii));
+	}
+	print_line(20, '-');
+	/* 从多签名脚本生成脚本公钥 */
+	scriptPubKey = GetScriptForDestination(mnScript);
+	std::cout<<"**** Bitcoin scriptPubKey:"<<std::endl;
+	for(int ii = 0; ii < scriptPubKey.size(); ii++){
+		std::cout<<std::setw(2);
+		std::cout<<std::setfill('0')<<std::hex<<(int)(*(scriptPubKey.begin() + ii));
+	}
+	print_line(20, '-');
+	/* 解析多签名脚本信息 */
+	txnouttype typeRet;							// 脚本类型 
+	std::vector<CTxDestination> addressRet;		// 脚本地址信息 
+	int nRequiredRet;							// 需要签名数量 
+	if(ExtractDestinations(mnScript, typeRet, addressRet, nRequiredRet)){
+		std::cout<<"**** Type:"<<GetTxnOutputType(typeRet)<<std::endl;
+		std::cout<<"**** Required: "<<nRequiredRet<<" / "<<addressRet.size();	
+		print_line(20, '-');	
+		for(int ii = 0; ii < addressRet.size(); ii++){
+			std::cout<<"**** Address script: "<<ii<<": "<<std::endl;
+			if(!IsValidDestination(addressRet[ii])){
+				break;
+			}
+			CScript script = GetScriptForDestination(addressRet[ii]);
+			P2WPKH = GetScriptForWitness(script);
+			for(int ij = 0; ij < P2WPKH.size(); ij++){
+				std::cout<<std::setw(2);
+				std::cout<<std::setfill('0')<<std::hex<<(int)(*(P2WPKH.begin() + ij));
+			}
+			print_line(20, '-');
+		}
+	}
+	return 0;
+} 
+/** 脚本生成测试程序结束 */ 
+
+int main(int argc, char** argv){
+	/** 公私钥测试程序 */
+	/* 
+	print_line(100, '='); 
+	std::cout<<"Private Key & Public Key";
+	print_line(100, '=');
+	main_keytest();
+	print_line(100, '=');
+	*/
+	/** 脚本生成测试程序 */ 
+	print_line(100, '=');
+	std::cout<<"Script";
+	print_line(100, '=');
+	main_scripttest();
+	print_line(100, '=');
+	
 	return 0;
 }
